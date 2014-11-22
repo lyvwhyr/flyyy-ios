5d66533ed6d784dd1041a563bfe333e7f4327fa5
Xingxing Xu <alex8xu@gmail.com>
2014-11-21 23:44:44 -0800

    Add recording animation

---
+++ Fly/ProductComponent/Record/FLYPrePostViewController.m
@@ -0,0 +1,37 @@
+//
+//  FLYPrePostViewController.m
+//  Fly
+//
+//  Created by Xingxing Xu on 11/20/14.
+//  Copyright (c) 2014 Fly. All rights reserved.
+//
+
+#import "FLYPrePostViewController.h"
+
+@interface FLYPrePostViewController ()
+
+@end
+
+@implementation FLYPrePostViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
--- Fly/ProductComponent/Record/FLYRecordViewController.m
+++ Fly/ProductComponent/Record/FLYRecordViewController.m
@@ -15,6 +15,7 @@
 #import "AERecorder.h"
 #import "AEAudioController.h"
 #import "FLYAudioStateManager.h"
+#import "FLYPrePostViewController.h"
 
 #define kInnerCircleRadius 100
 #define kOuterCircleRadius 150
@@ -34,13 +35,14 @@
 @property (nonatomic) PulsingHaloLayer *pulsingHaloLayer;
 
 @property (nonatomic) AEAudioController *audioController;
-@property (nonatomic) AEAudioFilePlayer *player;
+@property (nonatomic) AEAudioFilePlayer *audioPlayer;
 @property (nonatomic, copy) AudioPlayerCompleteblock completionBlock;
 
 @property (nonatomic, readonly) UITapGestureRecognizer *userActionTapGestureRecognizer;
 @property (nonatomic, readonly) UITapGestureRecognizer *deleteRecordingTapGestureRecognizer;
 
 @property (nonatomic) NSInteger recordedSeconds;
+@property (nonatomic) NSTimer *levelsTimer;
 
 @end
 
@@ -80,6 +82,56 @@
     };
 }
 
+- (void)updateLevels:(NSTimer*)timer
+{
+    Float32 inputAvg, inputPeak, outputAvg, outputPeak;
+    [_audioController inputAveragePowerLevel:&inputAvg peakHoldLevel:&inputPeak];
+    [_audioController outputAveragePowerLevel:&outputAvg peakHoldLevel:&outputPeak];
+    
+    float voicePower = translate(inputAvg, -20, 0);
+    
+    if (voicePower > 0) {
+        NSLog(@"voice: %f", voicePower);
+    }
+    
+    if (0<voicePower<=0.06) {
+        [self.userActionImageView setImage:[UIImage imageNamed:@"record_animate_01.png"]];
+    }else if (0.06<voicePower<=0.13) {
+        [self.userActionImageView setImage:[UIImage imageNamed:@"record_animate_02.png"]];
+    }else if (0.13<voicePower<=0.20) {
+        [self.userActionImageView setImage:[UIImage imageNamed:@"record_animate_03.png"]];
+    }else if (0.20<voicePower<=0.27) {
+        [self.userActionImageView setImage:[UIImage imageNamed:@"record_animate_04.png"]];
+    }else if (0.27<voicePower<=0.34) {
+        [self.userActionImageView setImage:[UIImage imageNamed:@"record_animate_05.png"]];
+    }else if (0.34<voicePower<=0.41) {
+        [self.userActionImageView setImage:[UIImage imageNamed:@"record_animate_06.png"]];
+    }else if (0.41<voicePower<=0.48) {
+        [self.userActionImageView setImage:[UIImage imageNamed:@"record_animate_07.png"]];
+    }else if (0.48<voicePower<=0.55) {
+        [self.userActionImageView setImage:[UIImage imageNamed:@"record_animate_08.png"]];
+    }else if (0.55<voicePower<=0.62) {
+        [self.userActionImageView setImage:[UIImage imageNamed:@"record_animate_09.png"]];
+    }else if (0.62<voicePower<=0.69) {
+        [self.userActionImageView setImage:[UIImage imageNamed:@"record_animate_10.png"]];
+    }else if (0.69<voicePower<=0.76) {
+        [self.userActionImageView setImage:[UIImage imageNamed:@"record_animate_11.png"]];
+    }else if (0.76<voicePower<=0.83) {
+        [self.userActionImageView setImage:[UIImage imageNamed:@"record_animate_12.png"]];
+    }else if (0.83<voicePower<=0.9) {
+        [self.userActionImageView setImage:[UIImage imageNamed:@"record_animate_13.png"]];
+    }else {
+        [self.userActionImageView setImage:[UIImage imageNamed:@"record_animate_14.png"]];
+    }
+    
+}
+
+static inline float translate(float val, float min, float max) {
+    if ( val < min ) val = min;
+    if ( val > max ) val = max;
+    return (val - min) / (max - min);
+}
+
 - (void)dealloc
 {
     NSLog(@"dealloc for record view controller");
@@ -100,12 +152,21 @@
     [backButton setImage:[UIImage imageNamed:@"icon_navigation_back"] forState:UIControlStateNormal];
     [backButton addTarget:self action:@selector(_backButtonTapped) forControlEvents:UIControlEventTouchUpInside];
     self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:backButton];
+    
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Next" style:UIBarButtonItemStylePlain target:self action:@selector(_nextBarButtonTapped)];
+    
 }
 
 - (void)_backButtonTapped
 {
     [self _cleanupData];
     [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)_nextBarButtonTapped
+{
+    FLYPrePostViewController *prePostVC = [FLYPrePostViewController new];
+    [self.navigationController pushViewController:prePostVC animated:YES];
 }
 
 - (void)_setupInitialViewState
@@ -149,6 +210,10 @@
     [self updateViewConstraints];
     
     [[FLYAudioStateManager manager] startRecord];
+    _audioPlayer = [FLYAudioStateManager manager].player;
+    _audioController = [FLYAudioStateManager manager].audioController;
+    
+    self.levelsTimer = [NSTimer scheduledTimerWithTimeInterval:0.05 target:self selector:@selector(updateLevels:) userInfo:nil repeats:YES];
 }
 
 - (void)_setupCompleteViewState
