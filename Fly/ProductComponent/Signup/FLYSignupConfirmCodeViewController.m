f227ba2fe90dbb7d00e9861aa096d6fe64a41a4a
Xingxing Xu <alex8xu@gmail.com>
2015-03-22 16:04:55 -0700

    Signup second step

--- Fly/ProductComponent/Signup/FLYSignupConfirmCodeViewController.m
+++ Fly/ProductComponent/Signup/FLYSignupConfirmCodeViewController.m
@@ -19,8 +19,6 @@
 
 @interface FLYSignupConfirmCodeViewController ()<UITextFieldDelegate>
 
-@property (nonatomic) UILabel *titleLabel;
-@property (nonatomic) UIImageView *phoneIconView;
 @property (nonatomic) UILabel *hintLabel;
 @property (nonatomic) UIView *inputPhoneView;
 @property (nonatomic) UIImageView *lockImageView;
@@ -37,31 +35,20 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor whiteColor];
-    
-//    self.titleLabel = [UILabel new];
-//    self.titleLabel.translatesAutoresizingMaskIntoConstraints = NO;
-//    self.titleLabel.font = [UIFont flyFontWithSize:21];
-//    self.titleLabel.textColor = [UIColor flyBlue];
-//    self.titleLabel.text = LOC(@"FLYConfirmCodeTitle");
-//    [self.view addSubview:self.titleLabel];
-    
-    self.phoneIconView = [UIImageView new];
-    self.phoneIconView.translatesAutoresizingMaskIntoConstraints = NO;
-    self.phoneIconView.image = [UIImage imageNamed:@"icon_login_phone_text"];
-    [self.view addSubview:self.phoneIconView];
+    self.view.backgroundColor = [UIColor flyBlue];
     
     self.hintLabel = [UILabel new];
     self.hintLabel.translatesAutoresizingMaskIntoConstraints = NO;
     self.hintLabel.font = [UIFont flyFontWithSize:21];
-    self.hintLabel.textColor = [UIColor flyColorFlySignupGrey];
+    self.hintLabel.textColor = [UIColor whiteColor];
     self.hintLabel.text = LOC(@"FLYConfirmCodeHint");
     [self.view addSubview:self.hintLabel];
     
     self.inputPhoneView = [UIView new];
+    self.inputPhoneView.backgroundColor = [UIColor whiteColor];
     self.inputPhoneView.translatesAutoresizingMaskIntoConstraints = NO;
     CGFloat borderWidth = 1.0/[FLYUtilities FLYMainScreenScale];
-    self.inputPhoneView.layer.borderColor = [UIColor flyColorFlySignupGrey].CGColor;
+    self.inputPhoneView.layer.borderColor = [UIColor whiteColor].CGColor;
     self.inputPhoneView.layer.borderWidth = borderWidth;
     [self.view addSubview:self.inputPhoneView];
     
@@ -96,19 +83,9 @@
 
 - (void)_addConstraints
 {
-//    [self.titleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
-//        make.centerX.equalTo(self.view);
-//        make.top.equalTo(self.view).offset(kTitleTopPadding + kStatusBarHeight + kNavBarHeight);
-//    }];
-    
-    [self.phoneIconView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.centerX.equalTo(self.view);
-        make.top.equalTo(self.view).offset(kTitleTopPadding + kStatusBarHeight + kNavBarHeight);
-    }];
-    
     [self.hintLabel mas_makeConstraints:^(MASConstraintMaker *make) {
         make.centerX.equalTo(self.view);
-        make.top.equalTo(self.phoneIconView.mas_bottom).offset(10);
+        make.top.equalTo(self.view).offset(kTitleTopPadding + kStatusBarHeight + kNavBarHeight);
     }];
     
     [self.inputPhoneView mas_makeConstraints:^(MASConstraintMaker *make) {
@@ -137,7 +114,7 @@
 {
     NSString *confirmCode = self.verificationCodeField.text;
     if (confirmCode.length == 6) {
-        self.confirmButton.backgroundColor = [UIColor flyBlue];
+        self.confirmButton.backgroundColor = [UIColor flyButtonGreen];
         [self.confirmButton setEnabled:YES];
     }
 }
