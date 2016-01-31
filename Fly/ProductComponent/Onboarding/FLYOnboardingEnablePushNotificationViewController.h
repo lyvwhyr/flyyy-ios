3c0c8cf050f3e6770dbe41fab8682348fcc7afad
Xingxing Xu <alex8xu@gmail.com>
2016-01-31 13:29:47 -0800

    Update splash page

---
+++ Fly/ProductComponent/Onboarding/FLYOnboardingEnablePushNotificationViewController.h
@@ -0,0 +1,13 @@
+//
+//  FLYOnboardingEnablePushNotificationViewController.h
+//  Flyy
+//
+//  Created by Xingxing Xu on 1/30/16.
+//  Copyright © 2016 Fly. All rights reserved.
+//
+
+#import "FLYUniversalViewController.h"
+
+@interface FLYOnboardingEnablePushNotificationViewController : FLYUniversalViewController
+
+@end
---
+++ Fly/ProductComponent/Onboarding/FLYOnboardingEnablePushNotificationViewController.m
@@ -0,0 +1,23 @@
+//
+//  FLYOnboardingEnablePushNotificationViewController.m
+//  Flyy
+//
+//  Created by Xingxing Xu on 1/30/16.
+//  Copyright © 2016 Fly. All rights reserved.
+//
+
+#import "FLYOnboardingEnablePushNotificationViewController.h"
+
+@interface FLYOnboardingEnablePushNotificationViewController ()
+
+@end
+
+@implementation FLYOnboardingEnablePushNotificationViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+
+@end
